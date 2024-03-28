fe78b46773407947fd5018ce46d14c071a941bfa
Will Johnson <19559137+WilliamJohnsonJr@users.noreply.github.com>
2024-03-28 00:22:33 -0400

    increase url lengths, restrict nullability, clean up forms

--- forms.py
+++ forms.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from enum import Enum
 from flask_wtf import FlaskForm as Form
 from wtforms import (
     StringField,
@@ -17,112 +18,64 @@ def my_genre_check(form, field):
         if genre not in [g.value for g in Genre]:
             raise ValidationError('Value must be one or more of: ' + ', '.join([genre.value for genre in Genre]))
 
-state_list = [
-                    "AL",
-                    "AK",
-                    "AZ",
-                    "AR",
-                    "CA",
-                    "CO",
-                    "CT",
-                    "DE",
-                    "DC",
-                    "FL",
-                    "GA",
-                    "HI",
-                    "ID",
-                    "IL",
-                    "IN",
-                    "IA",
-                    "KS",
-                    "KY",
-                    "LA",
-                    "ME",
-                    "MT",
-                    "NE",
-                    "NV",
-                    "NH",
-                    "NJ",
-                    "NM",
-                    "NY",
-                    "NC",
-                    "ND",
-                    "OH",
-                    "OK",
-                    "OR",
-                    "MD",
-                    "MA",
-                    "MI",
-                    "MN",
-                    "MS",
-                    "MO",
-                    "PA",
-                    "RI",
-                    "SC",
-                    "SD",
-                    "TN",
-                    "TX",
-                    "UT",
-                    "VT",
-                    "VA",
-                    "WA",
-                    "WV",
-                    "WI",
-                    "WY"
-                ]
-state_choices = [
-            ("AL", "AL"),
-            ("AK", "AK"),
-            ("AZ", "AZ"),
-            ("AR", "AR"),
-            ("CA", "CA"),
-            ("CO", "CO"),
-            ("CT", "CT"),
-            ("DE", "DE"),
-            ("DC", "DC"),
-            ("FL", "FL"),
-            ("GA", "GA"),
-            ("HI", "HI"),
-            ("ID", "ID"),
-            ("IL", "IL"),
-            ("IN", "IN"),
-            ("IA", "IA"),
-            ("KS", "KS"),
-            ("KY", "KY"),
-            ("LA", "LA"),
-            ("ME", "ME"),
-            ("MT", "MT"),
-            ("NE", "NE"),
-            ("NV", "NV"),
-            ("NH", "NH"),
-            ("NJ", "NJ"),
-            ("NM", "NM"),
-            ("NY", "NY"),
-            ("NC", "NC"),
-            ("ND", "ND"),
-            ("OH", "OH"),
-            ("OK", "OK"),
-            ("OR", "OR"),
-            ("MD", "MD"),
-            ("MA", "MA"),
-            ("MI", "MI"),
-            ("MN", "MN"),
-            ("MS", "MS"),
-            ("MO", "MO"),
-            ("PA", "PA"),
-            ("RI", "RI"),
-            ("SC", "SC"),
-            ("SD", "SD"),
-            ("TN", "TN"),
-            ("TX", "TX"),
-            ("UT", "UT"),
-            ("VT", "VT"),
-            ("VA", "VA"),
-            ("WA", "WA"),
-            ("WV", "WV"),
-            ("WI", "WI"),
-            ("WY", "WY"),
-        ]
+def my_string_strip_filter(form, field):
+    if field is not None and isinstance(field, str):
+        return field.strip()
+
+class UsStates(Enum):
+    AL = "AL"
+    AK = "AK"
+    AZ = "AZ"
+    AR = "AR"
+    CA = "CA"
+    CO = "CO"
+    CT = "CT"
+    DE = "DE"
+    DC = "DC"
+    FL = "FL"
+    GA = "GA"
+    HI = "HI"
+    ID = "ID"
+    IL = "IL"
+    IN = "IN"
+    IA = "IA"
+    KS = "KS"
+    KY = "KY"
+    LA = "LA"
+    ME = "ME"
+    MT = "MT"
+    NE = "NE"
+    NV = "NV"
+    NH = "NH"
+    NJ = "NJ"
+    NM = "NM"
+    NY = "NY"
+    NC = "NC"
+    ND = "ND"
+    OH = "OH"
+    OK = "OK"
+    OR = "OR"
+    MD = "MD"
+    MA = "MA"
+    MI = "MI"
+    MN = "MN"
+    MS = "MS"
+    MO = "MO"
+    PA = "PA"
+    RI = "RI"
+    SC = "SC"
+    SD = "SD"
+    TN = "TN"
+    TX = "TX"
+    UT = "UT"
+    VA = "VA"
+    VT = "VT"
+    WA = "WA"
+    WI = "WI"
+    WV = "WV"
+    WY = "WY"
+state_list = [state.value for state in UsStates]
+state_choices = [(state.value, state.value) for state in UsStates]
 
 class ShowForm(Form):
     artist_id = StringField("artist_id")
@@ -133,41 +86,36 @@ class ShowForm(Form):
 
 
 class VenueForm(Form):
-    name = StringField("name", validators=[DataRequired()])
-    city = StringField("city", validators=[DataRequired()])
+    name = StringField("name", validators=[DataRequired()], filters=[lambda x: my_string_strip_filter(None, x)])
+    city = StringField("city", validators=[DataRequired()], filters=[lambda x: my_string_strip_filter(None, x)])
     state = SelectField(
         "state",
         validators=[
             DataRequired(),
-            AnyOf(state_list),
+            my_genre_check,
         ],
         choices=state_choices,
     )
-    address = StringField("address", validators=[DataRequired()])
-    phone = StringField("phone")
-    image_link = StringField("image_link")
+    address = StringField("address", validators=[DataRequired()], filters=[lambda x: my_string_strip_filter(None, x)])
+    phone = StringField("phone", validators=[Regexp(regex="^\\d{3}-\\d{3}-\\d{4}$", message="Must match pattern 000-000-0000")],
+                        filters=[lambda x: my_string_strip_filter(None, x)])
+    image_link = StringField("image_link", validators=[Optional(), URL()], filters=[lambda x: my_string_strip_filter(None, x)])
     genres = SelectMultipleField(
         "genres",
         validators=[DataRequired(), my_genre_check],
         choices=[genre.value for genre in Genre],
     )
-    facebook_link = StringField("facebook_link", validators=[URL()])
-    website_link = StringField("website_link")
+    facebook_link = StringField("facebook_link", validators=[Optional(), URL()], filters=[lambda x: my_string_strip_filter(None, x)])
+    website_link = StringField("website_link", validators=[Optional(), URL()], filters=[lambda x: my_string_strip_filter(None, x)])
 
     seeking_talent = BooleanField("seeking_talent")
 
-    seeking_description = StringField("seeking_description")
+    seeking_description = StringField("seeking_description", filters=[lambda x: my_string_strip_filter(None, x)])
 
 class ArtistForm(Form):
-    name = StringField("name", validators=[DataRequired()])
-    def filter_name(form, field):
-        if field is not None and isinstance(field, str):
-            return field.strip()
+    name = StringField("name", validators=[DataRequired()], filters=[lambda x: my_string_strip_filter(None, x)])
 
-    city = StringField("city", validators=[DataRequired()])
-    def filter_city(form, field):
-        if field is not None and isinstance(field, str):
-            return field.strip()
+    city = StringField("city", validators=[DataRequired()], filters=[lambda x: my_string_strip_filter(None, x)])
 
     state = SelectField(
         "state",
@@ -180,16 +128,11 @@ class ArtistForm(Form):
 
     phone = StringField(
         "phone",
-        validators=[Regexp(regex="^\\d{3}-\\d{3}-\\d{4}$", message="Must match pattern 000-000-0000")]
+        validators=[Regexp(regex="^\\d{3}-\\d{3}-\\d{4}$", message="Must match pattern 000-000-0000")],
+        filters=[lambda x: my_string_strip_filter(None, x)]
     )
-    def filter_phone(form, field):
-        if field is not None and isinstance(field, str):
-            return field.strip()
 
-    image_link = StringField("image_link", validators=[Optional(), URL()])
-    def filter_image_link(form, field):
-        if field is not None and isinstance(field, str):
-            return field.strip()
+    image_link = StringField("image_link", validators=[Optional(), URL()], filters=[lambda x: my_string_strip_filter(None, x)])
 
     genres = SelectMultipleField(
         "genres",
@@ -197,19 +140,10 @@ class ArtistForm(Form):
         choices=[genre.value for genre in Genre],
     )
 
-    facebook_link = StringField("facebook_link", validators=[Optional(), URL()])
-    def filter_facebook_link(form, field):
-        if field is not None and isinstance(field, str):
-            return field.strip()
+    facebook_link = StringField("facebook_link", validators=[Optional(), URL()], filters=[lambda x: my_string_strip_filter(None, x)])
 
-    website_link = StringField("website_link", validators=[Optional(), URL()])
-    def filter_website_link(form, field):
-        if field is not None and isinstance(field, str):
-            return field.strip()
+    website_link = StringField("website_link", validators=[Optional(), URL()], filters=[lambda x: my_string_strip_filter(None, x)])
 
     seeking_venue = BooleanField("seeking_venue")
 
-    seeking_description = StringField("seeking_description")
-    def filter_seeking_description(form, field):
-        if field is not None and isinstance(field, str):
-            return field.strip()
+    seeking_description = StringField("seeking_description", filters=[lambda x: my_string_strip_filter(None, x)])
