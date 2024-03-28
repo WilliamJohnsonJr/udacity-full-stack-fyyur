70f5de9461b3d9b54236b9aca9f273ba7c8e91f4
Will Johnson <19559137+WilliamJohnsonJr@users.noreply.github.com>
2024-03-28 15:17:38 -0400

    autoformat app.py

--- app.py
+++ app.py
@@ -5,7 +5,17 @@
 import json
 from dateutil import parser
 import babel
-from flask import Flask, abort, jsonify, render_template, request, Response, flash, redirect, url_for
+from flask import (
+    Flask,
+    abort,
+    jsonify,
+    render_template,
+    request,
+    Response,
+    flash,
+    redirect,
+    url_for,
+)
 from flask_migrate import Migrate
 from flask_moment import Moment
 import logging
@@ -21,7 +31,7 @@ from sqlalchemy import func
 
 app = Flask(__name__)
 moment = Moment(app)
-app.config.from_object('config')
+app.config.from_object("config")
 db.init_app(app)
 migrate = Migrate(app, db)
 app.app_context().push()
@@ -30,34 +40,38 @@ app.app_context().push()
 # Filters.
 # ----------------------------------------------------------------------------#
 
-def format_datetime(value, format='medium'):
+
+def format_datetime(value, format="medium"):
     if isinstance(value, datetime):
         date = value
     else:
         date = parser.parse(value)
-    
-    if format == 'full':
-        format = "EEEE MMMM, d, y 'at' h:mma"
-    elif format == 'medium':
-        format = "EE MM, dd, y h:mma"
-    
-    return babel.dates.format_datetime(date, format, locale='en')
 
-app.jinja_env.filters['datetime'] = format_datetime
+    if format == "full":
+        format = "EEEE MMMM, d, y 'at' h:mma"
+    elif format == "medium":
+        format = "EE MM, dd, y h:mma"
+
+    return babel.dates.format_datetime(date, format, locale="en")
+
+
+app.jinja_env.filters["datetime"] = format_datetime
 
 # ----------------------------------------------------------------------------#
 # Controllers.
 # ----------------------------------------------------------------------------#
 
-@app.route('/')
+
+@app.route("/")
 def index():
-  return render_template('pages/home.html')
+    return render_template("pages/home.html")
 
 
 #  Venues
 #  ----------------------------------------------------------------
 
-@app.route('/venues')
+
+@app.route("/venues")
 def venues():
     # Adapted from the Udacity Knowledge Center post here: https://knowledge.udacity.com/questions/145968#148233
 
@@ -79,47 +93,53 @@ def venues():
                     "num_upcoming_shows": venue.upcoming_shows_count,
                 }
             )
-        data.append({
-            "city": area.city,
-            "state": area.state,
-            "venues": venue_list
-        })
-    return render_template('pages/venues.html', areas=data)
+        data.append({"city": area.city, "state": area.state, "venues": venue_list})
+    return render_template("pages/venues.html", areas=data)
 
-@app.route('/venues/search', methods=['POST'])
+
+@app.route("/venues/search", methods=["POST"])
 def search_venues():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for Hop should return "The Musical Hop".
-  # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
-  response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }
-  return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
+    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
+    # seach for Hop should return "The Musical Hop".
+    # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
+    response = {
+        "count": 1,
+        "data": [
+            {
+                "id": 2,
+                "name": "The Dueling Pianos Bar",
+                "num_upcoming_shows": 0,
+            }
+        ],
+    }
+    return render_template(
+        "pages/search_venues.html",
+        results=response,
+        search_term=request.form.get("search_term", ""),
+    )
 
-@app.route('/venues/<int:venue_id>')
+
+@app.route("/venues/<int:venue_id>")
 def show_venue(venue_id):
-  # shows the venue page with the given venue_id
-  venue = Venue.query.filter_by(id=venue_id).first()
-  past_shows = venue.past_shows
-  upcoming_shows = venue.upcoming_shows
-  genre_names = [genre.genre.value for genre in venue.genres]
-  venue_dict = venue.__dict__
-  venue_dict["genres"] = genre_names
-  venue_dict["past_shows"] = past_shows
-  venue_dict["past_shows_count"] = venue.past_shows_count
-  venue_dict["upcoming_shows"] = upcoming_shows
-  venue_dict["upcoming_shows_count"] = venue.upcoming_shows_count
-  return render_template('pages/show_venue.html', venue=venue_dict)
+    # shows the venue page with the given venue_id
+    venue = Venue.query.filter_by(id=venue_id).first()
+    past_shows = venue.past_shows
+    upcoming_shows = venue.upcoming_shows
+    genre_names = [genre.genre.value for genre in venue.genres]
+    venue_dict = venue.__dict__
+    venue_dict["genres"] = genre_names
+    venue_dict["past_shows"] = past_shows
+    venue_dict["past_shows_count"] = venue.past_shows_count
+    venue_dict["upcoming_shows"] = upcoming_shows
+    venue_dict["upcoming_shows_count"] = venue.upcoming_shows_count
+    return render_template("pages/show_venue.html", venue=venue_dict)
+
 
 #  Create Venue
 #  ----------------------------------------------------------------
 
-@app.route('/venues/create', methods=['GET'])
+
+@app.route("/venues/create", methods=["GET"])
 def create_venue_form():
     form = VenueForm()
     return render_template("forms/new_venue.html", form=form)
@@ -163,16 +183,14 @@ def create_venue_submission():
         db.session.close()
         # on successful db insert, flash success
         flash("Venue " + request.form["name"] + " was successfully listed!")
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     except Exception as error:
-        print('Error:', error)
+        print("Error:", error)
         # We rollback in case of an error. Even though there is the possibility of an venue ID
         # being orphaned in a rollback due to the flush above, this is not a concern.
         db.session.rollback()
         flash(
-            "An error occurred. Venue "
-            + request.form["name"]
-            + " could not be listed."
+            "An error occurred. Venue " + request.form["name"] + " could not be listed."
         )
         db.session.close()
         abort(400)
@@ -202,30 +220,38 @@ def delete_venue(venue_id):
 
 #  Artists
 #  ----------------------------------------------------------------
-@app.route('/artists')
+@app.route("/artists")
 def artists():
-  data = Artist.query.all()
-  return render_template('pages/artists.html', artists=data)
+    data = Artist.query.all()
+    return render_template("pages/artists.html", artists=data)
 
-@app.route('/artists/search', methods=['POST'])
+
+@app.route("/artists/search", methods=["POST"])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
-  }
-  return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
+    # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
+    # search for "band" should return "The Wild Sax Band".
+    response = {
+        "count": 1,
+        "data": [
+            {
+                "id": 4,
+                "name": "Guns N Petals",
+                "num_upcoming_shows": 0,
+            }
+        ],
+    }
+    return render_template(
+        "pages/search_artists.html",
+        results=response,
+        search_term=request.form.get("search_term", ""),
+    )
 
-@app.route('/artists/<int:artist_id>')
+
+@app.route("/artists/<int:artist_id>")
 def show_artist(artist_id):
     # shows the artist page with the given artist_id
-    
+
     artist = Artist.query.filter_by(id=artist_id).first()
     past_shows = artist.past_shows
     upcoming_shows = artist.upcoming_shows
@@ -236,31 +262,37 @@ def show_artist(artist_id):
     artist_dict["past_shows_count"] = artist.past_shows_count
     artist_dict["upcoming_shows"] = upcoming_shows
     artist_dict["upcoming_shows_count"] = artist.upcoming_shows_count
-    return render_template('pages/show_artist.html', artist=artist_dict)
+    return render_template("pages/show_artist.html", artist=artist_dict)
+
 
 def fetch_and_build_venue(venue_id: int):
-    venue = Venue.query.filter_by(id=venue_id).first()  # We use filter_by here because get is deprecated
+    venue = Venue.query.filter_by(
+        id=venue_id
+    ).first()  # We use filter_by here because get is deprecated
     genre_names = [genre.genre.value for genre in venue.genres]
     venue_dict = venue.__dict__
     venue_dict["genres"] = genre_names
     return venue_dict
 
+
 def add_genres_to_venue(venue_id: int, genres: list):
     selected_genres = genres
     if len(selected_genres):
-        data = list(
-            map(lambda g: {"venue_id": venue_id, "genre": g}, selected_genres)
-        )
+        data = list(map(lambda g: {"venue_id": venue_id, "genre": g}, selected_genres))
         for item in data:
             new_vg = VenueGenre(venue_id=item["venue_id"], genre=item["genre"])
             db.session.add(new_vg)
 
+
 def fetch_and_build_artist(artist_id: int):
-    artist = Artist.query.filter_by(id=artist_id).first()  # We use filter_by here because get is deprecated
+    artist = Artist.query.filter_by(
+        id=artist_id
+    ).first()  # We use filter_by here because get is deprecated
     genre_names = [genre.genre.value for genre in artist.genres]
     artist_dict = artist.__dict__
     artist_dict["genres"] = genre_names
     return artist_dict
+
 
 def add_genres_to_artist(artist_id: int, genres: list):
     selected_genres = genres
@@ -313,20 +345,22 @@ def edit_artist_submission(artist_id):
         flash("Artist " + form.name.data + " was successfully updated!")
         return redirect(url_for("show_artist", artist_id=artist_id))
     except Exception as error:
-        print('Error:', error)
+        print("Error:", error)
         db.session.rollback()
         db.session.close()
         flash("An error occurred. Artist " + form.name.data + " could not be updated.")
         abort(400)
 
-@app.route('/venues/<int:venue_id>/edit', methods=['GET'])
+
+@app.route("/venues/<int:venue_id>/edit", methods=["GET"])
 def edit_venue(venue_id):
     form = VenueForm()
     venue_dict = fetch_and_build_venue(venue_id)
     form.process(data=venue_dict)  # Hydrate the form with the current data from the db
-    return render_template('forms/edit_venue.html', form=form, venue=venue_dict)
+    return render_template("forms/edit_venue.html", form=form, venue=venue_dict)
 
-@app.route('/venues/<int:venue_id>/edit', methods=['POST'])
+
+@app.route("/venues/<int:venue_id>/edit", methods=["POST"])
 def edit_venue_submission(venue_id):
     form = VenueForm(request.form, meta={"csrf": False})
     if not form.validate_on_submit():
@@ -363,24 +397,27 @@ def edit_venue_submission(venue_id):
         flash("An error occurred. Venue " + form.name.data + " could not be updated.")
         abort(400)
 
+
 #  Create Artist
 #  ----------------------------------------------------------------
 
-@app.route('/artists/create', methods=['GET'])
-def create_artist_form():
-  form = ArtistForm()
-  return render_template('forms/new_artist.html', form=form)
 
-@app.route('/artists/create', methods=['POST'])
+@app.route("/artists/create", methods=["GET"])
+def create_artist_form():
+    form = ArtistForm()
+    return render_template("forms/new_artist.html", form=form)
+
+
+@app.route("/artists/create", methods=["POST"])
 def create_artist_submission():
     # Adapted from this answer on Udacity Knowledge: https://knowledge.udacity.com/questions/627743
-    form = ArtistForm(request.form, meta={'csrf': False})
+    form = ArtistForm(request.form, meta={"csrf": False})
     if not form.validate_on_submit():
         message = []
         for field, err in form.errors.items():
-            message.append(field + ' ' + '|'.join(err))
-        flash('Errors ' + str(message))
-        return render_template('forms/new_artist.html', form=form)
+            message.append(field + " " + "|".join(err))
+        flash("Errors " + str(message))
+        return render_template("forms/new_artist.html", form=form)
     # called upon submitting the new artist listing form
     artist = Artist(
         name=form.name.data,
@@ -401,18 +438,14 @@ def create_artist_submission():
         db.session.close()
         # on successful db insert, flash success
         flash("Artist " + form.name.data + " was successfully listed!")
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     except Exception as error:
-        print('Error:', error)
+        print("Error:", error)
         # We rollback in case of an error. Even though there is the possibility of an artist ID
         # being orphaned in a rollback due to the flush above, this is not a concern.
         db.session.rollback()
         db.session.close()
-        flash(
-            "An error occurred. Artist "
-            + form.name.data
-            + " could not be listed."
-        )
+        flash("An error occurred. Artist " + form.name.data + " could not be listed.")
         abort(400)
     # No finally block here since we must return or abort (which auto-raises) in the try and except blocks.
 
@@ -437,17 +470,17 @@ def create_shows():
 
 @app.route("/shows/create", methods=["POST"])
 def create_show_submission():
-    form = ShowForm(request.form, meta={'csrf': False})
+    form = ShowForm(request.form, meta={"csrf": False})
     if not form.validate_on_submit():
         message = []
         for field, err in form.errors.items():
-            message.append(field + ' ' + '|'.join(err))
-        flash('Errors ' + str(message))
+            message.append(field + " " + "|".join(err))
+        flash("Errors " + str(message))
         return render_template("forms/new_show.html", form=form)
     show = Show(
         artist_id=form.artist_id.data,
         venue_id=form.venue_id.data,
-        start_time=form.start_time.data
+        start_time=form.start_time.data,
     )
     try:
         db.session.add(show)
@@ -455,49 +488,51 @@ def create_show_submission():
         db.session.close()
         # on successful db insert, flash success
         flash("Show was successfully listed!")
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     except Exception as error:
-        print('Error:', error)
+        print("Error:", error)
         db.session.rollback()
         db.session.close()
-        flash('An error occurred. Show could not be listed.')
+        flash("An error occurred. Show could not be listed.")
         abort(400)
 
 
 @app.errorhandler(400)
 def handle_bad_request(error):
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
+
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('errors/404.html'), 404
+    return render_template("errors/404.html"), 404
+
 
 @app.errorhandler(500)
 def server_error(error):
-    return render_template('errors/500.html'), 500
+    return render_template("errors/500.html"), 500
 
 
 if not app.debug:
-    file_handler = FileHandler('error.log')
+    file_handler = FileHandler("error.log")
     file_handler.setFormatter(
-        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+        Formatter("%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]")
     )
     app.logger.setLevel(logging.INFO)
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
-    app.logger.info('errors')
+    app.logger.info("errors")
 
 # ----------------------------------------------------------------------------#
 # Launch.
 # ----------------------------------------------------------------------------#
 
 # Default port:
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
 
 # Or specify port manually:
-'''
+"""
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-'''
+"""
